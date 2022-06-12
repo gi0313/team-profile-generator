fd0cdf433802ae1b5394d03b8b3c8df936b01a42
Giovanni Claro <gio.claro316@gmail.com>
2022-06-12 07:44:01 -0400

    started questions

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # team-profile-generator
+
+## Description
+-By inputting 'node index' in the command prompt, the user will presented with a list of questions
+
+-The user will add manager information and any employees they choose
+
+-Information displayed will vary depending on whether they choose to add an engineer or an intern 
+
+-Once the user completes inputting information, they will be notified that an HTML page with the employee information has been created and be accessible in the src folder
+
+-When the webpage is opened all employee information(name, ID, email, github, school) will be displayed.
--- index.js
+++ index.js
@@ -1,3 +1,94 @@
 const inquirer = require('inquirer'); 
 const fs = require('fs');
 const Employee = require('./lib/Employee')
+
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter manager name.',
+         validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a manager name.');
+                    return false;
+            }
+        }
+    },
+    {
+        name: 'id',
+        message: 'What is your employee ID?',
+        validate: idInput => {
+             if (idInput) {
+                return true;
+            } else {
+                    console.log('Enter your employee ID number.');
+                return false;
+             }
+        }
+    },
+    {
+         type: 'input',
+         name: 'email',
+        message: 'What is your email?',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                 console.log('Enter your email!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Enter an office number.',
+        validate: officeInput => {
+            if (officeInput) {
+                return true;
+            } else {
+                 console.log('Enter an office number.');
+                return false;
+            }
+        }
+    },
+];
+
+const promptEmployees = employeeData => {
+    if (!employeeData.employ) {
+        employeeData.employ = [];
+    }
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What employee did you want to add?',
+            choices: ['Engineer, Intern, None']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?',
+            validate: projectInput => {
+                if (projectInput) {
+                    return true;
+                } else {
+                    console.log('PLease enter your name!');
+                    return false;
+                }
+            }
+        },
+    ])
+
+}
+
+function init() {
+    inquirer.prompt(questions)
+    .then(data => {
+        console.log(data)
+    })
+}
+
+init();
